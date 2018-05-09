eb4ede91ed60a5db709c1a1ff05c98032db8ecda
Drew Green <sa2@MacBookPro1.local>
2018-05-09 10:30:49 -0400

    Finalized HIBP Script

--- Check-Breaches.ps1
+++ Check-Breaches.ps1
@@ -1,11 +1,16 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
  function Check-HaveIBeenPwned ($email) {
     $uri = "https://haveibeenpwned.com/api/v2/breachedaccount/$email"
-    $breaches = ConvertFrom-Json -InputObject (Invoke-WebRequest -Uri $uri) 
-    foreach($breach in $breaches){
-        Write-Output ("$($email+','+$breach.Title+','+$breach.BreachDate+','+$breach.Domain)")
+    try{
+        Write-Output $email
+        $breaches = ConvertFrom-Json -InputObject (Invoke-WebRequest -Uri $uri)
+        foreach($breach in $breaches){
+            Write-Output ("$($email+','+$breach.Title+','+$breach.Domain+','+$breach.BreachDate)") >> ./BreachedAccounts.csv
+        }
+    }catch{
+        Write-Output $_.Exception
     }
-    Start-Sleep -Seconds 2 
 }
 
-Get-Mailbox |  % { $email = $_.PrimarySMTPAddress.ToString(); Check-HaveIBeenPwned($email) }
+Write-Output "Email,BreachTitle,Domain,Date" >> ./BreachedAccounts.csv
+Get-Mailbox |  % { $email = $_.PrimarySMTPAddress.ToString(); Check-HaveIBeenPwned($email); Start-Sleep -Seconds 2 }
